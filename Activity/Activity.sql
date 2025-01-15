1e143ac6feba45ad31dd945b026b16457d6033cf
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2025-01-15 10:09:34 +0100

    updated tasks

--- Activity/Activity.sql
+++ Activity/Activity.sql
@@ -1,1 +1,26 @@
-SELECT * FROM user_db.orders;
+use user_db;
+CREATE TABLE users (
+	Id SERIAL PRIMARY KEY,
+	Name VARCHAR(100),
+	Usersemail VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE products(
+	Id SERIAL PRIMARY KEY,
+    Name VARCHAR(100),
+    Price DOUBLE,
+    WholesalePrice DOUBLE,
+    ListPrice DOUBLE
+);
+
+CREATE TABLE orders(
+	OrderId SERIAL PRIMARY KEY,
+    CustomerId BIGINT UNSIGNED,
+    ProductId BIGINT UNSIGNED,
+    OrderDate DATE,
+    DueDate DATE,
+    ShippingDestination CHAR(100),
+    OrderQty DOUBLE,
+    FOREIGN KEY (CustomerId) REFERENCES users(Id),
+    FOREIGN KEY (ProductId) REFERENCES products(Id)
+);
